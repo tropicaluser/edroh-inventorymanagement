d1b44f2a07e6cb39de723f8cc2d46b958aa6d0b4
tropicaluser <hallbergstephaniie@gmail.com>
2024-11-04 18:57:19 +0100

    feat(server): products api

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -20,6 +21,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
