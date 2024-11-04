c9fde97ca086fa416520d9686c7d2f4fc52de02f
tropicaluser <hallbergstephaniie@gmail.com>
2024-11-04 18:14:11 +0100

    feat(client): add CardPurchaseSummary

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CardPopularProducts from "@/app/dashboard/CardPopularProducts";
+import CardPurchaseSummary from '@/app/dashboard/CardPurchaseSummary';
 import CardSalesSummary from '@/app/dashboard/CardSalesSummary';
 
 function Dashboard() {
@@ -8,7 +9,7 @@ function Dashboard() {
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
       <CardSalesSummary />
-      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500" />
+      <CardPurchaseSummary />
       <div className="row-span-3 bg-gray-500" />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
